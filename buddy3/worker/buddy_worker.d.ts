3afb224706aba0a4a64400eafd8c7c0144f13665
The Inc. JSG <jeansebastien.gonsette@gmail.com>
2025-05-26 22:59:49 +0200

    Missing worker

---
+++ buddy3/worker/buddy_worker.d.ts
@@ -0,0 +1,81 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * Schedules the game under control of the main application.
+ */
+export class Server {
+  free(): void;
+  /**
+   * Called by the JS side to create a new Server.
+   */
+  constructor();
+  /**
+   * Called by the JS side to open a ONNX NN model from `raw_onnx_data` and to store it
+   * under the model key `model_name`.
+   */
+  load_nn_model(model_name: string, raw_onnx_data: Uint8Array): void;
+  /**
+   * Called by the JS side to transmit the action selected by the human player
+   */
+  send_action(agent_handle: number, action_json: string): void;
+  /**
+   * Called by the JS side to schedule a new turn of the game.
+   * This will trigger calls to function [process_notification](Self::process_notification).
+   */
+  schedule(): Promise<void>;
+  /**
+   * Start a new game provided the following configuration:
+   * * `game`: the name of the game.
+   * * `agent_configs`: a JSON string describing the configuration of the different agents.
+   */
+  start_game(game: string, agent_configs: string, seed: bigint): Promise<boolean>;
+  /**
+   * Stop the current game.
+   */
+  stop_game(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_server_free: (a: number, b: number) => void;
+  readonly server_new: () => number;
+  readonly server_load_nn_model: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly server_send_action: (a: number, b: number, c: number, d: number) => void;
+  readonly server_schedule: (a: number) => any;
+  readonly server_start_game: (a: number, b: number, c: number, d: number, e: number, f: bigint) => any;
+  readonly server_stop_game: (a: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_6: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0c856b91a60c030b: (a: number, b: number) => void;
+  readonly closure135_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure4310_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ buddy3/worker/buddy_worker_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_server_free: (a: number, b: number) => void;
+export const server_new: () => number;
+export const server_load_nn_model: (a: number, b: number, c: number, d: number, e: number) => void;
+export const server_send_action: (a: number, b: number, c: number, d: number) => void;
+export const server_schedule: (a: number) => any;
+export const server_start_game: (a: number, b: number, c: number, d: number, e: number, f: bigint) => any;
+export const server_stop_game: (a: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0c856b91a60c030b: (a: number, b: number) => void;
+export const closure135_externref_shim: (a: number, b: number, c: any) => void;
+export const closure4310_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
